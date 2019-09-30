a0b1896b6fa9a6e272e4efbed9bb79574d779187
Salinder Sidhu <salinder.sid@gmail.com>
2019-09-30 00:42:31 -0400

    Added apollo client

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+    uri: "https://48p1r2roz4.sse.codesandbox.io/"
+});
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Shortlinks App</h1>
-            </div>
+            <ApolloProvider client={client}>
+                <div>
+                    <h1>Shortlinks App</h1>
+                </div>
+            </ApolloProvider>
         )
     }
 }
