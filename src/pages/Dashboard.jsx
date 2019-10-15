49a92754c9cc2e972120e7f1663ecafa6bc2eb18
Salinder Sidhu <salinder.sid@gmail.com>
2019-10-14 20:01:40 -0400

    Implemented link management table

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 import {
     Header,
     Container,
@@ -7,15 +8,34 @@ import {
     Grid,
     Segment,
     Table,
+    Loader,
     Checkbox,
     Button,
     Icon
 } from 'semantic-ui-react';
+import gql from 'graphql-tag';
 
 import ShortLinksHeader from '../components/Header';
 import ShortLinksFooter from '../components/Footer';
 
+const FETCH_LINKS = gql`
+    {
+        getLinks {
+            _id
+            name
+            longURL
+            shortURL
+            createdBy
+        }
+    }
+`;
+
 function Dashboard() {
+    const {
+        loading,
+        data 
+    } = useQuery(FETCH_LINKS);
+
     return (
         <Fragment>
             <ShortLinksHeader />
@@ -30,111 +50,84 @@ function Dashboard() {
                     <Grid.Column>
                         <Segment inverted color='blue'>
                             <Container textAlign='center'>
-                                Content
+                                Statistic 1
                             </Container>
                         </Segment>
                     </Grid.Column>
                     <Grid.Column>
                         <Segment inverted color='green'>
                             <Container textAlign='center'>
-                                Content
+                                Statistic 2
                             </Container>
                         </Segment>
                     </Grid.Column>
                     <Grid.Column>
                         <Segment inverted color='yellow'>
                             <Container textAlign='center'>
-                                Content
+                                Statistic 3
                             </Container>
                         </Segment>
                     </Grid.Column>
                     <Grid.Column>
                         <Segment inverted color='red'>
                             <Container textAlign='center'>
-                                Content
+                                Statistic 4
                             </Container>
                         </Segment>
                     </Grid.Column>
-                    <Table
-                        compact
-                        celled
-                        style={{
-                            paddingLeft: '0',
-                            paddingRight: '0',
-                            marginLeft: '1em',
-                            marginRight: '1em'
-                        }}
-                    >
-                        <Table.Header>
-                            <Table.Row>
-                                <Table.HeaderCell>Status</Table.HeaderCell>
-                                <Table.HeaderCell>Name</Table.HeaderCell>
-                                <Table.HeaderCell>Link</Table.HeaderCell>
-                                <Table.HeaderCell>Date Modified</Table.HeaderCell>
-                                <Table.HeaderCell>Actions</Table.HeaderCell>
-                            </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
-                            <Table.Row>
-                                <Table.Cell collapsing>
-                                    <Checkbox toggle defaultChecked/>
-                                </Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>
-                                    <Button icon>
-                                        <Icon name='copy' />
-                                    </Button>
-                                    <Button icon>
-                                        <Icon name='edit' />
-                                    </Button>
-                                    <Button icon color='red'>
-                                        <Icon name='delete' />
-                                    </Button>
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell collapsing>
-                                    <Checkbox toggle defaultChecked/>
-                                </Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>
-                                    <Button icon>
-                                        <Icon name='copy' />
-                                    </Button>
-                                    <Button icon>
-                                        <Icon name='edit' />
-                                    </Button>
-                                    <Button icon color='red'>
-                                        <Icon name='delete' />
-                                    </Button>
-                                </Table.Cell>
-                            </Table.Row>
-                            <Table.Row>
-                                <Table.Cell collapsing>
-                                    <Checkbox toggle defaultChecked/>
-                                </Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>Cell</Table.Cell>
-                                <Table.Cell>
-                                    <Button icon>
-                                        <Icon name='copy' />
-                                    </Button>
-                                    <Button icon>
-                                        <Icon name='edit' />
-                                    </Button>
-                                    <Button icon color='red'>
-                                        <Icon name='delete' />
-                                    </Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        </Table.Body>
-                    </Table>
                 </Grid>
+                <Table compact celled>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Status</Table.HeaderCell>
+                            <Table.HeaderCell>Name</Table.HeaderCell>
+                            <Table.HeaderCell>Original Link</Table.HeaderCell>
+                            <Table.HeaderCell>Date Modified</Table.HeaderCell>
+                            <Table.HeaderCell>Actions</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {loading ? (
+                            <Table.Row>
+                                <Table.Cell colSpan={5}>
+                                    <Loader active size='medium' inline='centered' />
+                                </Table.Cell>
+                            </Table.Row>
+                        ) : (
+                            <Fragment>
+                                {data &&
+                                    data.getLinks.map(link => (
+                                        <Table.Row key={link._id}>
+                                            <Table.Cell collapsing>
+                                                <Checkbox toggle defaultChecked />
+                                            </Table.Cell>
+                                            <Table.Cell>
+                                                {link.name}
+                                            </Table.Cell>
+                                            <Table.Cell>
+                                                {link.longURL}
+                                            </Table.Cell>
+                                            <Table.Cell>
+                                                {new Date().toLocaleString()}
+                                            </Table.Cell>
+                                            <Table.Cell>
+                                                <Button icon>
+                                                    <Icon name='copy' />
+                                                </Button>
+                                                <Button icon>
+                                                    <Icon name='edit' />
+                                                </Button>
+                                                <Button icon color='red'>
+                                                    <Icon name='delete' />
+                                                </Button>
+                                            </Table.Cell>
+                                        </Table.Row>
+                                    ))
+                                }
+                            </Fragment>
+                        )}
+                    </Table.Body>
+                </Table>
             </Container>
             <ShortLinksFooter />
         </Fragment>
