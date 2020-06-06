3e4774811016dc2400eaa87e962c90cdb07fb78e
Salinder Sidhu <salinder.sid@gmail.com>
2020-06-06 16:49:41 -0400

    Fixed format for utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,13 +1,13 @@
 module.exports = {
-  browserifyLink: link => {
+  browserifyLink: (link) => {
     return !link.match(/^(http|https):\/\//i) ? `http://${link}` : link;
   },
-  copyToClipboard: textToCopy => {
+  copyToClipboard: (textToCopy) => {
     const tempElement = document.createElement("textarea");
     tempElement.value = textToCopy;
     document.body.appendChild(tempElement);
     tempElement.select();
     document.execCommand("copy");
     document.body.removeChild(tempElement);
-  }
+  },
 };
